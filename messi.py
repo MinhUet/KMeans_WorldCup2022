0936e161191525fe6af00b8b02ab97ead54cd3f4
MinhUet <buivanminh1309@gmail.com>
2022-12-19 01:04:42 +0700

    feat: Bổ sung constant

--- messi.py
+++ messi.py
@@ -6,9 +6,13 @@ img = plt.imread("messi.png")
 
 width = img.shape[0]
 height = img.shape[1]
+dimension = img.shape[2]
 
-img = img.reshape(width*height,4)
-kmeans = KMeans(n_clusters=8).fit(img)
+# Thay đổi cụm để có màu tốt nhất
+K = 4 
+
+img = img.reshape(width*height,dimension)
+kmeans = KMeans(n_clusters=K).fit(img)
 
 labels = kmeans.predict(img)
 clusters = kmeans.cluster_centers_
@@ -18,7 +22,7 @@ img2 = numpy.zeros_like(img)
 for i in range(len(img2)):
 	img2[i] = clusters[labels[i]]
 
-img2 = img2.reshape(width,height,4)
+img2 = img2.reshape(width,height,dimension)
 
 plt.imshow(img2)
 plt.show()
